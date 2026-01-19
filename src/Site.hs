26f8f23620313d3bea9f6e6e568508929bced3d8
codgician <15964984+codgician@users.noreply.github.com>
2026-01-19 17:37:44 +0800

    feat: add bilingual homepage

--- src/Site.hs
+++ src/Site.hs
@@ -5,7 +5,10 @@ module Site
   )
 where
 
+import Config (loadConfig)
+import Context (langCtx, siteCtx)
 import Hakyll
+import System.FilePath (takeFileName)
 
 config :: Configuration
 config =
@@ -17,12 +20,40 @@ config =
     }
 
 hakyllMain :: IO ()
-hakyllMain = hakyllWith config $ do
-  -- Copy static files
-  match "static/**" $ do
-    route $ gsubRoute "static/" (const "")
-    compile copyFileCompiler
+hakyllMain = do
+  cfg <- loadConfig "config.yaml"
+  hakyllWith config $ do
+    -- Copy static files (excluding SCSS source files)
+    match ("static/**" .&&. complement "static/scss/**") $ do
+      route $ gsubRoute "static/" (const "")
+      compile copyFileCompiler
 
-  -- Compile templates
-  match "templates/*" $ compile templateBodyCompiler
-  match "templates/partials/*" $ compile templateBodyCompiler
+    -- Compile SCSS to CSS
+    match "static/scss/style.scss" $ do
+      route $ constRoute "css/style.css"
+      compile $
+        getResourceString
+          >>= withItemBody (unixFilter "sass" ["--stdin", "--style=compressed"])
+
+    -- Compile templates
+    match "templates/*" $ compile templateBodyCompiler
+    match "templates/partials/*" $ compile templateBodyCompiler
+
+    -- Homepage (per language)
+    match "content/index.*.md" $ do
+      route $ customRoute $ \ident ->
+        let path = toFilePath ident
+            lang = takeWhile (/= '.') $ drop 1 $ dropWhile (/= '.') $ takeFileName path
+         in lang <> "/index.html"
+      compile $ do
+        lang <- getLang
+        getResourceBody
+          >>= applyAsTemplate (langCtx lang <> siteCtx cfg)
+          >>= loadAndApplyTemplate "templates/home.html" (langCtx lang <> siteCtx cfg)
+
+getLang :: Compiler String
+getLang = do
+  ident <- getUnderlying
+  let path = toFilePath ident
+      lang = takeWhile (/= '.') $ drop 1 $ dropWhile (/= '.') $ takeFileName path
+  pure lang
