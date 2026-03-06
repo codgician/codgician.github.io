9289841f5a29657113d4ee773d4f68bc59da85ba
codgician <15964984+codgician@users.noreply.github.com>
2026-03-06 13:54:27 +0800

    ci: unify linting under nix flake check
    - Add checks attribute to flake.nix (hlint, ormolu, tests, build)
    - Simplify lint.yml to just run 'nix flake check'
    - Fix hlint warnings (redundant do, use <$>)
    - Format code with ormolu

--- flake.nix
+++ flake.nix
@@ -217,6 +217,33 @@
           '';
         };
 
+        # Checks for CI (run with `nix flake check`)
+        checks = {
+          # HLint check
+          hlint = pkgs.runCommand "hlint-check" {
+            nativeBuildInputs = [ hPkgs.hlint ];
+          } ''
+            cd ${builderSrc}
+            hlint src/ app/ test/
+            touch $out
+          '';
+
+          # Ormolu formatting check
+          ormolu = pkgs.runCommand "ormolu-check" {
+            nativeBuildInputs = [ hPkgs.ormolu ];
+          } ''
+            cd ${builderSrc}
+            ormolu --mode check $(find src app test -name '*.hs')
+            touch $out
+          '';
+
+          # Haskell tests (run via cabal)
+          tests = siteBuilder;
+
+          # Build check (ensures site builds)
+          build = website;
+        };
+
         formatter = pkgs.writeShellApplication {
           name = "formatter";
           runtimeInputs = with pkgs; [
